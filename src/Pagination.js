706656fc18765d91da2be202a7c4455f2cbd4ed1
Sanda Mandresy <hei.sanda.3@gmail.com>
2022-05-23 20:00:53 +0300

    feat : adding new files about tables structures

---
+++ src/Pagination.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Pagination() {
+    return(<>
+        <nav className='dataTable-pagination'>
+            <ul className='dataTable-pagination-list'>
+                <li className='active'><a href='#' data-page="1">1</a></li>
+            </ul>
+        </nav>
+</>);
+}
---
+++ src/columns.js
@@ -0,0 +1,27 @@
+export const COLUMNS = [
+    {
+        Header : "Name",
+        accessor : "Name"
+    },
+    {
+        Header :"Position",
+        accessor :"Position"
+    },
+    {
+        Header : "Office",
+        accessor : "Office"
+    },
+    {
+        Header : "Age",
+        accessor : "Age"
+    },
+    {
+        Header : "Start date",
+        accessor : "Start date"
+    },
+    {
+        Header : "Salary",
+        accessor: "Salary"
+    }
+    
+];
