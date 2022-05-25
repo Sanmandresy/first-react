afb21875b09ace44d5fc0f918ce09fa356d26e6b
Sanda Mandresy <hei.sanda.3@gmail.com>
2022-05-25 19:38:37 +0300

    fix : removing footer

--- src/columns.js
+++ src/columns.js
@@ -1,32 +1,26 @@
 export const COLUMNS = [
     {
         Header : "Name",
-        Footer : "Name",
         accessor : "Name"
     },
     {
         Header :"Position",
-        Footer : "Position",
         accessor :"Position"
     },
     {
         Header : "Office",
-        Footer : "Office",
         accessor : "Office"
     },
     {
         Header : "Age",
-        Footer : "Age",
         accessor : "Age"
     },
     {
         Header : "Start date",
-        Footer : "Start date",
         accessor : "Start date"
     },
     {
         Header : "Salary",
-        Footer : "Salary",
         accessor: "Salary"
     }
     
